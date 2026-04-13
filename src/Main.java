4d0e7643cc5d7919fd25c8ca16c32028ae01d75a
Seishin Kato <seisinkato@gmail.com>
2026-04-13 10:41:31 -0500

    setup scaffolding

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+// to see how IntelliJ IDEA suggests fixing it.
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        website.web(args); // starts website
+
+
+
+    }
+}
---
+++ src/website.java
@@ -0,0 +1,81 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class website {
+    public static void web (String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        server.createContext("/", new website.HelloHandler());
+
+        // 2. NEW: This tells the server how to handle the request for your JS file
+        server.createContext("/pdfview.js", new website.JSHandler());
+
+        server.createContext("/pdf.js", new PDFLibHandler());
+        // If your pdfview.js is looking for "sample.pdf"
+        server.createContext("/pdf.pdf", new PDFFileHandler());
+
+        server.setExecutor(null);
+
+        server.start();
+        System.out.println("server staterd MF!!!!");
+
+    }
+    static class HelloHandler implements HttpHandler{
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException{
+
+            byte[] response = Files.readAllBytes(Paths.get("src/index.html"));
+            //String response = "hello";
+
+            exchange.sendResponseHeaders(200,response.length);
+
+            OutputStream os = exchange.getResponseBody();
+
+            os.write(response);
+            os.close();
+        }
+    }
+    // NEW: HANDLER FOR JAVASCRIPT
+    static class JSHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            // Make sure pdfview.js is inside your 'src' folder!
+            byte[] response = Files.readAllBytes(Paths.get("src/pdfview.js"));
+
+            // IMPORTANT: We tell the browser this is a JavaScript file
+            exchange.getResponseHeaders().set("Content-Type", "application/javascript");
+
+            exchange.sendResponseHeaders(200, response.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response);
+            os.close();
+        }
+    }
+    static class PDFLibHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            byte[] response = Files.readAllBytes(Paths.get("src/pdf.js-master/src/pdf.js"));
+            exchange.getResponseHeaders().set("Content-Type", "application/javascript");
+            exchange.sendResponseHeaders(200, response.length);
+            exchange.getResponseBody().write(response);
+            exchange.getResponseBody().close();
+        }
+    }
+    static class PDFFileHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            // Change "src/sample.pdf" to whatever your file is actually named!
+            byte[] response = Files.readAllBytes(Paths.get("src/pdf.pdf"));
+
+            exchange.getResponseHeaders().set("Content-Type", "application/pdf");
+            exchange.sendResponseHeaders(200, response.length);
+            exchange.getResponseBody().write(response);
+            exchange.getResponseBody().close();
+        }
+    }
+}
